ad6fe046a6faa8a9b07ff6921e9fbb0a5a82bd49
pinareray <pinareray3@gmail.com>
2024-05-10 00:43:58 +0300

    Data types in dart language were examined

---
+++ main.dart
@@ -0,0 +1,18 @@
+
+void main(List<String> args) {
+  print("Hello");
+
+  String hello = "Hello5";
+  print(hello);
+  print(hello);
+  print(hello);
+  print(hello);
+  print(hello);
+
+  int money = 15;
+  print(money);
+
+  print(hello + '' + '$money');
+
+
+}
---
+++ week1/user_basic.dart
@@ -0,0 +1,25 @@
+
+void main(List<String> args) {
+  
+//keep customer's name
+//find out customer's money
+//Say hello to the customer and tell him his money.
+//Then add five dollars to his money.Replace this added money with ten.
+
+int userMoney = 25;
+String userName = "Veli";
+userMoney = userMoney + 5;
+
+print("Hello $userName $userMoney");
+
+print('-----');
+userMoney = userMoney - 10; // or userMoney -=10 
+
+print('Your money has lost value. $userMoney');
+
+
+double newMoney = 15.2;
+newMoney = newMoney/2;
+print('Your money has lost value. $newMoney');
+
+}
